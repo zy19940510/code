083427d6c7067fd9585ba9a681a4a494220745ad
jerry <nicejerry666@gmail.com>
2021-04-28 15:06:33 +0800

    feat: 增加数组类型题目

---
+++ 算法题/数组/最大子序和.js
@@ -0,0 +1,23 @@
+// 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+function maxSubArray(nums) {
+  let ans = nums[0];
+  let sum = 0;
+  for (const num of nums) {
+    if (sum > 0) sum += num;
+    if (sum <= 0) sum = num;
+    ans = Math.max(ans, sum);
+  }
+  return ans;
+}
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
+
+/**
+ * 思路
+ * 遍历当前数组，当前连续子序列的和为sum，结果为ans
+ * 如果sum > 0，则说明 sum + nums[i] > nums[i]，意思就是加上当前这一项可以使sum更大，所以就加上num[i]
+ * 如果sum < 0, 则说明加上当前这一项会使总和变小，所以就舍弃当前项
+ * 每次比较ans和sum的大小，将最大值ans更新
+ * 遍历结束返回结果
+ * 时间复杂度O(n)
+ */
