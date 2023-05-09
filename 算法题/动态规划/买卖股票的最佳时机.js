1cb7043c82d2a6f231b81f94dea64154379e87b9
zhangyu295 <zhangyu295@meituan.com>
2023-05-09 17:38:32 +0800

    feat: 新增贪心

--- 算法题/动态规划/买卖股票的最佳时机.js
+++ 算法题/动态规划/买卖股票的最佳时机.js
@@ -6,9 +6,9 @@
  * 第i天持有 由 第i-1天持有然后不操作 和 第i-1天不持有然后买入 两种情况的最大值转移过来
  * dp[i][1] = Math.max(dp[i - 1][1], -prices[i])
  */
-
-//时间复杂度O(n) 空间复杂度O(n)，dp数组第二维是常数
-const maxProfit = function (prices) {
+// 1、动态规划
+//时间复杂度O(n) 空间复杂度O(n)
+var maxProfit = function (prices) {
   let n = prices.length;
   let dp = Array.from(Array(n), () => Array(2));
   dp[0][0] = 0; //第0天不持有
@@ -19,9 +19,10 @@ const maxProfit = function (prices) {
   }
   return dp[n - 1][0];
 };
-// 状态压缩，可以把dp[i]去掉，变为一维，让空间复杂度降为O(1)
+
+// 状态压缩，因为dp[i]只和dp[i-1]有关，所以可以压缩空间复杂度到O(1)
 // 时间复杂度O(n) 空间复杂度O(1)
-const maxProfit = function (prices) {
+var maxProfit = function (prices) {
   let n = prices.length;
   let dp = Array.from(Array(n), () => Array(2));
   dp[0] = 0;
@@ -33,14 +34,14 @@ const maxProfit = function (prices) {
   return dp[0];
 };
 
-// 语意化
-const maxProfit = function (prices) {
-  let n = prices.length;
-  let sell = 0;
-  let buy = -prices[0];
-  for (let i = 1; i < n; i++) {
-    sell = Math.max(sell, buy + prices[i]);
-    buy = Math.max(buy, -prices[i]);
-  }
-  return sell;
-};
+// 2、贪心算法，空间复杂度O(1) 时间复杂度O(n)
+var maxProfit = function (prices) {
+    if (prices.length < 2) return 0;
+    let min = prices[0]; // 最小买入点
+    let max = 0; // 最大卖出点
+    for(let p of prices) {
+        min = Math.min(min, p);
+        max = Math.max(max, p - min);
+    }
+    return max;
+}
